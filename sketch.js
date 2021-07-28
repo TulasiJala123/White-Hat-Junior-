5a14a9368230253d6ef1df2e7eccabe666652e11
TulasiJala123 <87837047+TulasiJala123@users.noreply.github.com>
2021-07-28 18:55:01 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,37 +1,37 @@
-var movingRect, fixedRect;
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
 
-function setup() {
-  createCanvas(1200,800);
-  fixedRect = createSprite(600,400,50,80);
-  fixedRect.shapeColor = "green";
-  fixedRect.debug = true;
-   movingRect = createSprite(400,200,80,30);
-   movingRect.shapeColor = "green";
-   movingRect.debug = true;
+var engine, world;
+var ground, ball;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    var ground_options ={
+        isStatic: true
+    }
+    var ball_options = {
+        restitution:1.0, 
+    }
+    
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    console.log(ground);
 }
 
-function draw() {
-  background(255,255,255); 
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
-   isTouching();
-  
-  drawSprites();
-}
-
-function isTouching() {
-
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2 
-    && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2
-     && movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2 
-    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) { 
-       movingRect.shapeColor = "red"; 
-       fixedRect.shapeColor = "red"; 
-      }
-      else {
-        movingRect.shapeColor = "green"; 
-        fixedRect.shapeColor = "green";
-      }  
-
-
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+    
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
+    
 }
